4cdcc0deb6e6fb38235d195568ca1a3135937092
HUNGNAM\truon <truongnhuhungnam@gmail.com>
2022-09-24 17:56:19 +0700

    wip

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from "../features/posts/postsSlide";
 
 export const store = configureStore({
-  reducer: {
-    
-  },
+    reducer: {
+        posts: postsReducer,
+    },
 });
--- src/features/posts/index.js
+++ src/features/posts/index.js
@@ -1,6 +1,10 @@
+import { useSelector, useDispatch } from "react-redux";
+import { getAllPosts } from "./postsSlide";
+
 const Posts = () => {
-  return (
-    <div>Posts List</div>
-  )
-}
-export default Posts
+    const posts = useSelector(getAllPosts);
+    console.log(posts);
+
+    return <div>Posts List</div>;
+};
+export default Posts;
