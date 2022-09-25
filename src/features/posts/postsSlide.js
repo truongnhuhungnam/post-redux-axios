442a346c83f80d14a9708df0af354a2cd67fb192
HUNGNAM\truon <truongnhuhungnam@gmail.com>
2022-09-25 22:11:35 +0700

    WIP

--- src/features/posts/postsSlide.js
+++ src/features/posts/postsSlide.js
@@ -1,19 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { postsApi } from "../../api/index";
 
-export function fetchPosts() {
-    return async (dispatch) => {
-        postsApi
-            .get("/posts")
-            .then((response) => {
-                dispatch(setPosts(response.data));
-            })
-            .catch((er) => {
-                dispatch(setError());
-            });
-    };
-}
-
 const initialState = {
     loading: false,
     error: false,
@@ -27,10 +14,10 @@ const postsSlide = createSlice({
         setLoading: (state) => {
             state.loading = true;
         },
-        setPosts: (state, { payload }) => {
+        setPosts: (state, action) => {
             state.loading = false;
             state.error = false;
-            state.posts = payload;
+            state.posts = action.payload;
         },
         setError: (state) => {
             state.error = true;
@@ -43,3 +30,16 @@ export const { setLoading, setPosts, setError } = postsSlide.actions;
 export const postsSelector = (state) => state.posts;
 
 export default postsSlide.reducer;
+
+export function fetchPosts() {
+    return async (dispatch) => {
+        postsApi
+            .get("/posts")
+            .then((response) => {
+                dispatch(setPosts(response.data));
+            })
+            .catch((er) => {
+                dispatch(setError());
+            });
+    };
+}
