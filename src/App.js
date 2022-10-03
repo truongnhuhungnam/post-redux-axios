81db435092225450fc6637346beedb83070ade9a
HUNGNAM\truon <truongnhuhungnam@gmail.com>
2022-10-03 23:00:18 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import AddPostForm from "./features/posts/AddPostForm";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route index element={<PostsList />} />
-      <Route path="post">
-        <Route index element={<AddPostForm />} />
-        <Route path=":postId" element={<SinglePostPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+        </Route>
       </Route>
     </Routes>
   );
